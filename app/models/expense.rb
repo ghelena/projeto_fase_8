45159f189eb60fa5854b2061ac579b6edcf013fd
andre-tlima <andre-tlima@hotmail.com>
2019-04-27 13:34:20 +0000

    Adicionando v2, ransack e algumas melhorias

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,5 +1,5 @@
 class Expense < ApplicationRecord
-  belongs_to :user
-
-  validates_presence_of :description, :user_id
+    belongs_to :user
+    
+    validates_presence_of :description, :user_id
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,23 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+    # Include default devise modules. Others available are:
+    # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+    devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+    validates_uniqueness_of :auth_token
+    before_create :generate_authentication_token!
+    has_many :gains, dependent: :destroy
+    has_many :expenses, dependent: :destroy
+
+    def info
+        "#{email} - #{create_at} - Token #{Devise.friendly_token}"
+    end
+
+    def generate_authentication_token!
+        begin
+            self.auth_token = Devise.friendly_token
+        end while User.exists?(auth_token: auth_token)
+    end
     
-  validates_uniqueness_of :auth_token
-  before_create :generate_authentication_token!
-  has_many :gains, dependent: :destroy
-  has_many :expenses, dependent: :destroy    
-   
-  def info
-     "#{email} - #{create_at} - Token #{Devise.friendly_token}" 
-  end
-    
-  def generate_authentication_token!
-     begin
-         self.auth_token = Devise.friendly_token
-     end while User.exists?(auth_token: auth_token)     
-  end      
 end
+    
